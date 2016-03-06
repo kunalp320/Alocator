21f38fc0ee36fca6b0ee883cbe68cceda7afdab1
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-03-06 15:50:09 -0800

    Tons of errors, kind of need help I think.

--- Alocator/ViewController.swift
+++ Alocator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var beerPercentTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var numberOfBeers: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func beerTextFieldDidChange(sender: UITextField) {
+        let floatValue = Double(sender.text!)
+        if floatValue == 0 {
+            sender.text = nil
+        }
+    }
+    
+    @IBAction func sliderValueDidChange(sender: UISlider) {
+        self.beerPercentTextField.resignFirstResponder()
+    }
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        self.beerPercentTextField.resignFirstResponder()
+        
+        let numberOfBeers : Float = self.numberOfBeers.value
+        let ouncesInOneBeerGlass : Int = 12
+        
+        let alcoholPercentageOfBeer : Double = Double(self.beerPercentTextField!)! / 100
+        let ouncesOfAlcoholPerBeer : Double = ouncesInOneBeerGlass * alcoholPercentageOfBeer
+    
+    }
+    
+    
+    
 
 }
 
